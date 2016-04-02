6f884407ee1e602942d7cca8684666f9e4da7401
Austin Colcord <ajdcolcord@gmail.com>
2016-04-02 19:57:06 -0400

    higher heartbeat timeout

--- Server.py
+++ Server.py
@@ -9,7 +9,7 @@ class Server:
         self.replica_ids = replica_ids
         self.election_timeout = random.randint(150, 300)
         self.election_timeout_start = datetime.datetime.now()
-        self.heartbeat_timeout = 50
+        self.heartbeat_timeout = 100
         self.heartbeat_timeout_start = datetime.datetime.now()
         self.current_term = 0
         self.voted_for = None
