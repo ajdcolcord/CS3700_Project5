66ad3df53672cd9b24640d77f1f010ce98600950
Austin Colcord <ajdcolcord@gmail.com>
2016-04-08 13:59:59 -0400

    asdl;kfj

--- Server.py
+++ Server.py
@@ -238,7 +238,7 @@ class Server:
             # if len(self.log) - 1 > leader_prev_log_index:
             print str(self.id) + " COMPARING LEADERPREVLOGTERM " + str(leader_prev_log_term) + " TO MY TERM " + str(self.log[leader_prev_log_index][1])
             if self.log[leader_prev_log_index][1] == leader_prev_log_term:
-                self.log = self.log[:leader_prev_log_index] + logEntry['entries']
+                self.log = self.log[:leader_prev_log_index + 1] + logEntry['entries']
                 self.commit_index = len(self.log) - 1
                 reply = {'src': self.id,
                          'dst': message['src'],
