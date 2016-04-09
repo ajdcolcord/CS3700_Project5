bf1a06a264a84c1654e9decf29b0679c3cf49bfd
Austin Colcord <ajdcolcord@gmail.com>
2016-04-09 13:19:43 -0400

    test

--- Server.py
+++ Server.py
@@ -349,8 +349,8 @@ class Server:
             # if len(self.log) - 1 > leader_prev_log_index:
             print str(self.id) + " COMPARING LEADERPREVLOGTERM " + str(leader_prev_log_term) + " TO MY TERM " + str(self.log[leader_prev_log_index][1])
             if self.log[leader_prev_log_index][1] == leader_prev_log_term:
-                #self.log = self.log[:leader_prev_log_index + 1] + logEntry['entries']
-                self.log = self.log[:leader_prev_log_index] + logEntry['entries']
+                self.log = self.log[:leader_prev_log_index + 1] + logEntry['entries']
+                # self.log = self.log[:leader_prev_log_index] + logEntry['entries']
 
                 print str(self.id) + ": ADDED ENTRIES INTO FOLLOWER LOG: " + str(self.log)
                 self.commit_index = len(self.log) - 1
