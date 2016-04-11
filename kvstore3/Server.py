c071e2e68b977ed458eb6781e5cbf9deadc7729d
Austin Colcord <ajdcolcord@gmail.com>
2016-04-11 10:30:12 -0400

    setting high heartbeat

--- kvstore3/Server.py
+++ kvstore3/Server.py
@@ -21,7 +21,7 @@ class Server:
         self.replica_ids = replica_ids
         self.election_timeout = random.randint(150, 300)
         self.election_timeout_start = datetime.datetime.now()
-        self.heartbeat_timeout = self.election_timeout / 2
+        self.heartbeat_timeout = 140  # self.election_timeout / 2
         self.heartbeat_timeout_start = datetime.datetime.now()
         self.current_term = 0
         self.voted_for = None
