eebe7924623ed916abfeb67cefccdb416515a866
Austin Colcord <ajdcolcord@gmail.com>
2016-04-14 17:19:34 -0400

    len log

--- kvstore-new/Server.py
+++ kvstore-new/Server.py
@@ -311,7 +311,7 @@ class Server:
                               "leader": self.leader_id,
                               "type": "append_entries_rpc_ack",
                               "term": self.currentTerm,
-                              "match_index": self.last_applied} #(self.log)}
+                              "match_index": len(self.log)} #(self.log)}
 
         self.send(append_entries_rpc)
 
@@ -327,7 +327,7 @@ class Server:
                               "leader": self.leader_id,
                               "type": "append_entries_rpc_ack",
                               "term": self.currentTerm,
-                              "match_index": self.last_applied}
+                              "match_index": len(self.log)}
                               #"match_index": max(0, leader_prev_log_index)}
 
         self.send(append_entries_rpc)
