b4ce769d97265b8c99e240611427b5b253c45030
Austin Colcord <ajdcolcord@gmail.com>
2016-04-16 14:55:30 -0400

    revert

--- kvstore-new/Server.py
+++ kvstore-new/Server.py
@@ -48,13 +48,7 @@ class Server:
                             print str(self.id) + " I am a Leader, becoming a follower of " + str(msg['src']) + " who's log size is larger than mine!"
                             self.become_follower(msg['src'], msg['term'])
                 else:
-                    if len(self.log) <= msg['lastLogIndex'] + 1 and self.node_state == "L":
-                        print str(self.id) + " I am a Cand or Leader, becoming a follower of " + str(
-                            "FFFF") + " because of a vote who's log size is larger than mine!"
-
-                        self.become_follower("FFFF", msg['term'])
-                        
-                    elif len(self.log) < msg['lastLogIndex'] and self.node_state != "L":
+                    if len(self.log) <= msg['lastLogIndex']:
                         if not self.node_state == "F":
                             print str(self.id) + " I am a Cand or Leader, becoming a follower of " + str(
                                 "FFFF") + " because of a vote who's log size is larger than mine!"
