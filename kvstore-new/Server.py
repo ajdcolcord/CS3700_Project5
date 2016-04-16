48165af2acb81114245492910281b974e2a1b5d6
Austin Colcord <ajdcolcord@gmail.com>
2016-04-16 12:51:39 -0400

    prev log term change in send_append_entries rpc individual

--- kvstore-new/Server.py
+++ kvstore-new/Server.py
@@ -260,7 +260,7 @@ class Server:
         prevLogTerm = 0
         if len(self.log): #and self.match_index[replica_id] > 0:
             # prevLogTerm = self.log[self.match_index[replica_id] - 1][1]
-            prevLogTerm = self.log[self.match_index[replica_id]][1]
+            prevLogTerm = self.log[self.match_index[replica_id] - 1][1]
 
         entries = self.log[self.match_index[replica_id]: self.match_index[replica_id] + 50]
         if len(self.log) > 1 and self.log[self.match_index[replica_id]] > 1:
