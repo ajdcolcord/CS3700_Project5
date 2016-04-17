8a538b5e8e873deb41909981a04e65c71b9d8288
Austin Colcord <ajdcolcord@gmail.com>
2016-04-17 18:40:11 -0400

    try this

--- kvstore-new_alternate/Server.py
+++ kvstore-new_alternate/Server.py
@@ -53,7 +53,7 @@ class Server:
                         print str(self.id) + " I am a Cand or Leader, becoming a follower of " + str(
                             "FFFF") + " because of a vote who's log size is larger than mine!"
 
-                        self.become_follower("FFFF", msg['term'])
+                        self.become_follower(msg['src'], msg['term'])
                     elif self.node_state == "F":
                         print str(self.id) + " I am a Follower, Becoming *le Follower"
                         #self.become_follower(self.leader_id, msg['term'])
