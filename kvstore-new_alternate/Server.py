7a61dc3cf86e3d56e49e7c4de56830536a7ed76f
Austin Colcord <ajdcolcord@gmail.com>
2016-04-17 18:56:23 -0400

    Last Applied

--- kvstore-new_alternate/Server.py
+++ kvstore-new_alternate/Server.py
@@ -340,7 +340,7 @@ class Server:
                               "leader": self.leader_id,
                               "type": "append_entries_rpc_ack",
                               "term": self.currentTerm,
-                              "match_index": len(self.log)}
+                              "match_index": self.last_applied}
                               #"match_index": max(0, leader_prev_log_index)}
 
         self.send(append_entries_rpc)
